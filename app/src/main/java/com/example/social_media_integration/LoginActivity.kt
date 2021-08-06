70f2bdce26751111a7b8a389a8353dc993ff588f
Bandhan singh <parbhatbandhan@gmail.com>
2021-08-06 14:13:40 +0530

    i have uploaded social media integration app on github

---
+++ app/src/main/java/com/example/social_media_integration/LoginActivity.kt
@@ -0,0 +1,88 @@
+package com.example.social_media_integration
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.ErrorCodes
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+
+    companion object {
+        private const val RC_SIGN_IN = 1234
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        createSignInIntent()
+
+    }
+
+    private fun createSignInIntent() {
+        val providers = arrayListOf<AuthUI.IdpConfig>(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.FacebookBuilder().build(),
+            AuthUI.IdpConfig.PhoneBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setIsSmartLockEnabled(false)
+                .setTheme(R.style.GreenTheme)
+                .setLogo(R.drawable.tsf_logo)
+                .build(),
+            RC_SIGN_IN
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == RC_SIGN_IN){
+            var response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK){
+                val user = FirebaseAuth.getInstance().currentUser
+                user?.displayName?.let { Log.d("USERDATA", it) }
+                val intent = Intent(this,MainActivity::class.java)
+//                val image = user?.photoUrl
+//
+//                intent.putExtra("USERNAME", user?.displayName)
+//                intent.putExtra("USEREMAIL",user?.email)
+//                intent.putExtra("USERPHONE",user?.phoneNumber)
+//                intent.putExtra("USERPROVIDER",user?.providerId)
+//                intent.putExtra("USERIMAGE",user?.photoUrl)
+                startActivity(intent)
+
+            }
+
+            else{
+
+                if(response == null){
+                    finish()
+                }
+                if (response?.getError()?.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    //Show No Internet Notification
+                    return
+                }
+
+                if(response?.getError()?.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    Toast.makeText(this, response?.error?.errorCode.toString(), Toast.LENGTH_LONG)
+                        .show()
+                    Log.d("ERRORCODE", response?.error?.errorCode.toString())
+                    return
+                }
+            }
+        }
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/social_media_integration/MainActivity.kt
@@ -0,0 +1,91 @@
+package com.example.social_media_integration
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
+import com.example.social_media_integration.databinding.ActivityMainBinding
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    private val auth = FirebaseAuth.getInstance().currentUser
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        if(auth!=null && intent!=null){
+            createUI()
+        }
+        else{
+            startActivity(Intent(this,LoginActivity::class.java))
+            this.finish()
+        }
+
+        binding.logoutBtn.setOnClickListener {
+            AuthUI.getInstance().signOut(this).addOnSuccessListener {
+                Toast.makeText(this,"Logged out successfully",Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this,LoginActivity::class.java))
+            }
+        }
+
+
+
+    }
+
+    private fun createUI(){
+
+        auth?.let {
+            binding.txtName.text = auth.displayName
+            binding.txtemail.text = auth.email
+            binding.txtPhoneNumber.text = auth.phoneNumber
+
+            Glide.with(this)
+                .load(auth.photoUrl)
+                .fitCenter()
+                //.placeholderDrawable(R.drawable.profile_img)
+                .into(binding.profileImage)
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if(auth!=null && intent!=null){
+            createUI()
+        }
+        else{
+            startActivity(Intent(this,LoginActivity::class.java))
+            this.finish()
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finishAffinity()
+    }
+
+
+    //generateFBKey()  // in oncreate
+    //    fun generateFBKey(){
+//        try {
+//            val info = packageManager.getPackageInfo(
+//                "com.example.social_media_integration",
+//                PackageManager.GET_SIGNATURES
+//            )
+//            for (signature in info.signatures) {
+//                val md: MessageDigest = MessageDigest.getInstance("SHA")
+//                md.update(signature.toByteArray())
+//                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+//            }
+//        } catch (e: PackageManager.NameNotFoundException) {
+//        } catch (e: NoSuchAlgorithmException) {
+//        }
+//    }
+}
